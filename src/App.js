e96fb9e867733ad76ab8240788c048cb881e33cb
Jisoo Han <llqrw55@naver.com>
2023-08-03 14:41:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import {Container,Nav,Navbar,Row,Col} from 'react-bootstrap';
+import 대문이미지 from './img/bg.png'
+import data from './data';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [shoes ,setShoes]=useState(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">SooMarker</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{backgroundImage:'url('+대문이미지+')'}}></div>
+      <Container>
+      <Row>
+      <Shoes shoes={shoes}/>
+      <Shoes shoes={shoes}/>
+      <Shoes shoes={shoes}/>
+      </Row>
+    </Container>
     </div>
   );
+
+  function Shoes(props){
+    return(
+      <Col md={4} >
+        <img src={props.shoes[0].img} width='80%'/>
+        <h4>{props.shoes[0].title}</h4>
+        <p>{props.shoes[0].price}</p>
+      </Col>
+    )
+  }
 }
 
 export default App;
