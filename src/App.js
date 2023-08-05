a30719a326a35114ae9f7faacbd656180a7875d1
JIS0098 <llqrw55@naver.com>
2023-08-05 18:07:01 +0900

    네스트드 라우터 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link,useNavigate, Outlet } from 'react-router-dom';
 import 대문이미지 from './img/bg.png'
 import data from './data';
 import './App.css';
@@ -10,6 +10,7 @@ import Detail from './page/Detail';
 function App() {
   let [shoes, setShoes] = useState(data)
   let [count, setCount] = useState()
+  let navigate = useNavigate();
 
 
   return (
@@ -18,8 +19,8 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">SooMarker</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -37,13 +38,27 @@ function App() {
           </Container>
         </div>} />
         <Route path='/detail' element={<Detail/>} />
-
+        <Route path='*' element={<div>요청할수 없는 페이지 입니다.</div>}/>
+        <Route path='/event' element={<Event/>}>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}/>
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>}/>
+        </Route>
       </Routes>
 
 
 
     </div>
   );
+
+  function Event(){
+    return(
+      <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+    )
+  }
+          
 
   function Card(props) {
     return (
