8ca7e88c0e827d75418562fe08439fbb750e34d8
JIS0098 <llqrw55@naver.com>
2023-08-05 17:21:39 +0900

    디테일 페이지까지

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import {Container,Nav,Navbar,Row,Col} from 'react-bootstrap';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
 import 대문이미지 from './img/bg.png'
 import data from './data';
 import './App.css';
 import { useState } from 'react';
+import Detail from './page/Detail';
+
 
 function App() {
-  let [shoes ,setShoes]=useState(data)
-  let [count,setCount]=useState()
+  let [shoes, setShoes] = useState(data)
+  let [count, setCount] = useState()
+
+
   return (
     <div className="App">
-       <Navbar bg="light" data-bs-theme="light">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">SooMarker</Navbar.Brand>
           <Nav className="me-auto">
@@ -18,24 +23,32 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <Routes>
+        <Route path='/' element={<div>
+          <div className='main-bg' style={{ backgroundImage: 'url(' + 대문이미지 + ')' }}></div>
+          <Container>
+            <Row>
+              {
+                shoes.map((a) => {
+                  return (<Card shoes={a} />)
+                })
+              }
+            </Row>
+          </Container>
+        </div>} />
+        <Route path='/detail' element={<Detail/>} />
 
-      <div className='main-bg' style={{backgroundImage:'url('+대문이미지+')'}}></div>
-      <Container>
-      <Row>
-        {
-          shoes.map((a)=>{
-            return(<Card shoes={a}/>)
-          })
-        }
-      </Row>
-    </Container>
+      </Routes>
+
+
+
     </div>
   );
 
-  function Card(props){
-    return(
+  function Card(props) {
+    return (
       <Col md={4} >
-        <img src={props.shoes.img} width='80%'/>
+        <img src={props.shoes.img} width='80%' />
         <h4>{props.shoes.title}</h4>
         <p>{props.shoes.price}</p>
       </Col>
