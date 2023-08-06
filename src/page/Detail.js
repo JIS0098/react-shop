4f429210526c71488e6ec7644de5c216488e0d38
Jisoo Han <llqrw55@naver.com>
2023-08-06 22:32:43 +0900

    유즈 이팩트 숙제

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,27 +1,38 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const Detail = (props) => {
-    let {id} = useParams();
-    let  선택상품 = props.shoes.find((a)=>{
-        return a.id==id})
+  let { id } = useParams();
+  let 선택상품 = props.shoes.find((a) => {
+    return a.id == id;
+  });
+ 
+
+  useEffect(() => {
+
+  }, []);
   return (
     <div>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src={선택상품.img} width="100%"/>
-        </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">{선택상품.title}</h4>
-          <p>{선택상품.content}</p>
-          <p>{선택상품.price}</p>
-          <button className="btn btn-danger">주문하기</button>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={선택상품.img} width="100%" />
+          </div>
+          <div className="col-md-6">
+         
+              <div className="alert alert-danger">숫자를 입력해주세요.</div>:null
+            
+            <input onChange={(e)=>{}} type="text" placeholder="갯수를 입력해주세요."></input>
+            <h4 className="pt-5">{선택상품.title}</h4>
+            <p>{선택상품.content}</p>
+            <p>{선택상품.price}</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
